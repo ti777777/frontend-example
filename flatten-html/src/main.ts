f2a23c7b4c867f3040151fba9e642c44885bc00f
ti777777 <ti12272198686@yahoo.com.tw>
2022-08-02 13:42:56 +0800

    update

--- flatten-html/src/main.ts
+++ flatten-html/src/main.ts
@@ -1,6 +1,5 @@
-
 var initElement = document.getElementById("app");
-var json = mapDOM(initElement, true);
+//var json = mapDOM(initElement, true);
 
 function mapDOM(element: Node, json: boolean) {
   var treeObject = {};
@@ -24,26 +23,29 @@ function mapDOM(element: Node, json: boolean) {
       if (nodeList.length) {
         object["element"] = [];
         object["children"] = [];
-        nodeList.forEach((x: Node, i: number) => {
-          if (nodeList[i].nodeType == Node.TEXT_NODE) {
-            let nodeValue = nodeList[i].nodeValue?.replaceAll(/\s|\n/gm, "");   
+        nodeList.forEach((x: Node) => {
+          if (x.nodeType == Node.TEXT_NODE) {
+            let nodeValue = x.nodeValue?.replaceAll(/\s|\n/gm, "");
             if (nodeValue != "") {
-              let element = 
-              {
-                type : nodeList[i].parentElement,
-                content : nodeValue
-              };
-              console.log(element)
-              object["element"].push(element);
+              object["element"].push({
+                type: x.parentElement,
+                content: nodeValue,
+              });
             }
+          } else if (x instanceof HTMLVideoElement) {
+            object["element"].push({
+              type: "video",
+              content: x.src,
+            });
           } else {
             object["children"].push({});
             treeHTML(
-              nodeList[i] as Element,
+              x as Element,
               object["children"][object["children"].length - 1]
             );
           }
         });
+
         if (object["children"].length == 0) {
           delete object["children"];
         }
@@ -65,3 +67,57 @@ function mapDOM(element: Node, json: boolean) {
   return treeObject;
 }
 
+class DomObject {
+  public container?: HTMLElement | null;
+  public content?: string | null;
+  public children?: DomObject[];
+  constructor() {}
+}
+
+function traveseNode(root: Node): DomObject {
+  let domObject: DomObject = new DomObject();
+  let container!: HTMLElement | null;
+
+  if (root.nodeType == Node.TEXT_NODE) {
+    let nodeValue = root.nodeValue?.replaceAll(/\s|\n/gm, "");
+    if (nodeValue != "") {
+      container = root.parentElement;
+      domObject.content = nodeValue;
+    }
+  } else {
+    if (
+      root instanceof HTMLImageElement ||
+      root instanceof HTMLVideoElement
+    ) {
+      container = root.parentElement;
+      domObject.content = root.src;
+    } else {
+      if (root.childNodes && root.childNodes.length > 0) {
+        domObject.children = [];
+        root.childNodes.forEach((child) => {
+          let c = traveseNode(child);
+          if (c.content || c.children) {
+            domObject.children?.push(c);
+          }
+        });
+      }
+    }
+  }
+
+  if(!domObject.content){
+    delete domObject.content
+  }
+
+  if(!domObject.children){
+    delete domObject.children
+  }
+
+  domObject.container = container;
+
+  return domObject;
+}
+
+let target = document.getElementById("app");
+let parser, docNode;
+
+console.log(traveseNode(target as Node));
