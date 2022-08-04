17dc3833d5586aa78d6ee651453801eed5092b1c
ti777777 <ti12272198686@yahoo.com.tw>
2022-08-05 00:42:58 +0800

    update

--- BlockyEditor/src/lib/index.ts
+++ BlockyEditor/src/lib/index.ts
@@ -5,12 +5,10 @@ export class Blocky {
   handle!: HTMLElement;
   model!: BlockyModel;
   view!: BlockyTemplate;
-  children!: Blocky[];
   constructor() {}
   static fromHtml(handle: HTMLElement, src: Node): Blocky {
     let ret: Blocky = new Blocky();
     let model: BlockyModel = BlockyModel.convertHtmlToModel(src);
-    ret.children = [];
     ret.model = model;
     ret.view = new BlockyTemplate(handle, model);
     return ret;
